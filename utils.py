9f951c8f831510c2a7d5b0e5fb00fcbf6c10d9a6
SAPPHIR3-ROS3 <patriziosimone@hotmail.it>
2024-05-25 12:05:09 +0200

    added log function

---
+++ utils.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+ERROR_LOG_LEVEL = 0
+DEBUG_LOG_LEVEL = 1
+INFO_LOG_LEVEL = 2
+RESULT_LOG_LEVEL = 3
+
+RED = '\033[91m'
+YELLOW = '\033[93m'
+CYAN = '\033[96m'
+GREEN = '\033[92m'
+GRAY = '\033[90m'
+END = '\033[0m'
+
+redText = lambda text: f'{RED}{text}{END}'
+yellowText = lambda text: f'{YELLOW}{text}{END}'
+cyanText = lambda text: f'{CYAN}{text}{END}'
+greenText = lambda text: f'{GREEN}{text}{END}'
+grayText = lambda text: f'{GRAY}{text}{END}'
+
+logColor = {
+    ERROR_LOG_LEVEL: redText,
+    DEBUG_LOG_LEVEL: yellowText,
+    INFO_LOG_LEVEL: greenText,
+    RESULT_LOG_LEVEL: cyanText
+}
+
+logLevel = {
+    ERROR_LOG_LEVEL: 'ERROR',
+    DEBUG_LOG_LEVEL: 'DEBUG',
+    INFO_LOG_LEVEL: 'INFO',
+    RESULT_LOG_LEVEL: 'RESULT'
+}
+
+def log(currentLogLevel, level, message, parameters: dict = None):
+    if currentLogLevel not in logLevel.keys():
+        raise ValueError('Invalid log level')
+    
+    if level > currentLogLevel:
+        return
+
+    date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    color = logColor[level]
+    type = color(logLevel[level])
+    message = color(message)
+    parameters = f'| {grayText(str(parameters))}' if parameters else None 
+
+    logMessage = f'[{date}][{type}] {message} {parameters if parameters else ""}'
+
+    print(logMessage)
